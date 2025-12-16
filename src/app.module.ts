fb77d368eeff9784b1581f9eaf0fb1485dbdd787
Giuliano Contino <continogiuliano627@gmail.com>
2025-12-16 16:09:21 -0300

    feat: import temp-sensor module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {DatabaseModule} from './database/database.module';
+import {TempSensorModule} from './temp-sensor/temp-sensor.module';
 
 @Module({
-	imports: [DatabaseModule],
+	imports: [DatabaseModule, TempSensorModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
