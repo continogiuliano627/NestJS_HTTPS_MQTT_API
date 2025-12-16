71f2a69f80cc23236c6f3636381a2ecc4cf10d58
Giuliano Contino <continogiuliano627@gmail.com>
2025-12-16 04:03:30 -0300

    feat: added better-sqlite3 db

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 const bootstrap = async (): Promise<void> => {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4627);
-}
+	const app = await NestFactory.create(AppModule);
+	await app.listen(process.env.API_PORT ?? 4625);
+};
 void bootstrap();
