9ccc8031d8674091491121b688a72385d77fc252
Giuliano Contino <continogiuliano627@gmail.com>
2025-12-16 00:30:42 -0300

    config: finished initial config

---
+++ .eslintrc.cjs
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+
+  parser: "@typescript-eslint/parser",
+
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: "./tsconfig.eslint.json"
+  },
+
+  env: {
+    node: true,
+    es2021: true
+  },
+
+  plugins: ["@typescript-eslint", "prettier", "unused-imports"],
+
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended"
+  ],
+
+  rules: {
+    /* Limpieza */
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+    ],
+    "unused-imports/no-unused-imports": "error",
+
+    /* Types */
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-misused-promises": "error",
+
+    /* Prettier */
+    "prettier/prettier": ["error"]
+  },
+
+  overrides: [
+    {
+      files: ["*.cjs", "*.js"],
+      parserOptions: {
+        project: null
+      }
+    }
+  ],
+
+  ignorePatterns: ["node_modules", "dist"]
+};
