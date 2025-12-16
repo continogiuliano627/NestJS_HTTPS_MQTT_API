ea5765440d698a258409dff439924a97646939b7
Giuliano Contino <continogiuliano627@gmail.com>
2025-12-16 04:11:48 -0300

    format: applied new prettier format settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,67 +1,67 @@
 module.exports = {
-  root: true,
+	root: true,
 
-  parser: "@typescript-eslint/parser",
+	parser: '@typescript-eslint/parser',
 
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module",
-    project: "./tsconfig.eslint.json"
-  },
+	parserOptions: {
+		ecmaVersion: 2021,
+		sourceType: 'module',
+		project: './tsconfig.eslint.json'
+	},
 
-  env: {
-    node: true,
-    es2021: true
-  },
+	env: {
+		node: true,
+		es2021: true
+	},
 
-  plugins: ["@typescript-eslint", "prettier", "unused-imports"],
+	plugins: ['@typescript-eslint', 'prettier', 'unused-imports'],
 
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended"
-  ],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:prettier/recommended'
+	],
 
-  rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "off",
+	rules: {
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
 
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_"
-      }
-    ],
+		'unused-imports/no-unused-imports': 'error',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{
+				vars: 'all',
+				varsIgnorePattern: '^_',
+				args: 'after-used',
+				argsIgnorePattern: '^_'
+			}
+		],
 
-    /* Types */
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/consistent-type-imports": "error",
-    "@typescript-eslint/no-floating-promises": "error",
-    "@typescript-eslint/no-misused-promises": "error",
+		/* Types */
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'@typescript-eslint/consistent-type-imports': 'error',
+		'@typescript-eslint/no-floating-promises': 'error',
+		'@typescript-eslint/no-misused-promises': 'error',
 
-    /* Prettier */
-    "prettier/prettier": "error"
-  },
+		/* Prettier */
+		'prettier/prettier': 'error'
+	},
 
-  overrides: [
-    {
-      files: ["src/**/*.controller.ts", "src/**/*.service.ts", "src/**/*.module.ts"],
-      rules: {
-        "@typescript-eslint/consistent-type-imports": "off"
-      }
-    },
-    {
-      files: ["*.cjs", "*.js"],
-      parserOptions: {
-        project: null
-      }
-    }
-  ],
+	overrides: [
+		{
+			files: ['src/**/*.controller.ts', 'src/**/*.service.ts', 'src/**/*.module.ts'],
+			rules: {
+				'@typescript-eslint/consistent-type-imports': 'off'
+			}
+		},
+		{
+			files: ['*.cjs', '*.js'],
+			parserOptions: {
+				project: null
+			}
+		}
+	],
 
-  ignorePatterns: ["node_modules", "dist"]
+	ignorePatterns: ['node_modules', 'dist']
 };
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: "better-sqlite3",
-            database: "db.sqlite",
-            autoLoadEntities: true,
-            synchronize: false
-        })
-    ]
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'better-sqlite3',
+			database: 'db.sqlite',
+			autoLoadEntities: true,
+			synchronize: false
+		})
+	]
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
