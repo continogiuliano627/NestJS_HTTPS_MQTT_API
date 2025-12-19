4bf1e76c7a355677c9ddb0d78014916343ee870e
Giuliano Contino <continogiuliano627@gmail.com>
2025-12-19 14:33:15 -0300

    feat: added swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import {NestFactory} from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import {AppModule} from './app.module';
 
 const bootstrap = async (): Promise<void> => {
 	const app = await NestFactory.create(AppModule);
+	const swaggerConfig = new DocumentBuilder()
+		.setTitle('IoT Server API')
+		.setDescription('API for a IoT set with HTTPS interface & mosquitto mqtt broker')
+		.setVersion('1.0.0')
+		.build();
+	const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+	SwaggerModule.setup('docs', app, swaggerDocument);
 	await app.listen(process.env.API_PORT ?? 4625);
 };
 void bootstrap();
