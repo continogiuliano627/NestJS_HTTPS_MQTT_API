4c73ae9c3c194e24ba9056d52812f793f519d9d8
Giuliano Contino <continogiuliano627@gmail.com>
2025-12-25 20:53:41 -0300

    feat: imported new module 'User'

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {DatabaseModule} from './database/database.module';
 import {TempSensorModule} from './temp-sensor/temp-sensor.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-	imports: [DatabaseModule, TempSensorModule],
+	imports: [DatabaseModule, TempSensorModule, UserModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
