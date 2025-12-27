67e4f3e4b13e7954a96851a49e74c0854bdf899e
Giuliano Contino <continogiuliano627@gmail.com>
2025-12-27 13:17:28 -0300

    feat: added userexample, added decorators to dto's

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,6 +1,27 @@
-import {type UserRole} from 'src/global/enum';
+import {IsEnum, IsOptional, IsString} from 'class-validator';
+import {UserRole} from 'src/global/enum';
 
-export type CreateUserDto = {
-	name: string;
-	role: UserRole;
+export const UserExample = {
+	id: 'abcd1234-56ef-89gh-10ij-klmn1112opqr1314',
+	name: 'Username',
+	role: 'user',
+	createdAt: '1997-22-12T12:13:00Z',
+	updatedAt: '2025-22-12T12:13:00Z'
 };
+
+export class CreateUserDto {
+	@IsString()
+	name: string;
+	@IsEnum(UserRole)
+	role: UserRole;
+}
+
+export class UpdateUserDto {
+	@IsString()
+	@IsOptional()
+	name?: string;
+	@IsString()
+	@IsOptional()
+	@IsEnum(UserRole)
+	role?: UserRole;
+}
