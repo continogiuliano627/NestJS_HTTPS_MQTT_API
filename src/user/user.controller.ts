87c936ebfd86b38af3c5fd10036aa83242ca38dd
Giuliano Contino <continogiuliano627@gmail.com>
2025-12-27 13:18:00 -0300

    feat: added endpoints to get all, by name, by id, by role, delete one & update one

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,19 @@
-import {Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post} from '@nestjs/common';
-import {ApiBody, ApiOperation, ApiParam, ApiResponse} from '@nestjs/swagger';
-import {CreateUserDto} from './dto/user.dto';
+import {
+	BadRequestException,
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	ParseUUIDPipe,
+	Patch,
+	Post,
+	Query
+} from '@nestjs/common';
+import {ApiBody, ApiOperation, ApiParam, ApiQuery, ApiResponse} from '@nestjs/swagger';
+import {User} from 'src/database/entities/user.entity';
+import {UserRole} from 'src/global/enum';
+import {CreateUserDto, UpdateUserDto, UserExample} from './dto/user.dto';
 import {UserService} from './user.service';
 
 @Controller('user')
@@ -25,6 +38,112 @@ export class UserController {
 	})
 	getAll() {
 		return this.userService.getAll();
+	}
+
+	@Get('/getById/:id')
+	@ApiOperation({
+		summary: 'Get 1 user by ID'
+	})
+	@ApiParam({
+		name: 'id',
+		type: 'string',
+		required: true
+	})
+	@ApiResponse({
+		status: 200,
+		description: 'User found',
+		type: User,
+		example: UserExample
+	})
+	getById(@Param('id', new ParseUUIDPipe()) id: string) {
+		return this.userService.findById(id);
+	}
+
+	@Get('/getByName/')
+	@ApiOperation({
+		summary: 'Get 1 user by Name'
+	})
+	@ApiQuery({
+		name: 'name',
+		type: 'string',
+		required: true
+	})
+	@ApiResponse({
+		status: 200,
+		description: 'User found',
+		type: User,
+		example: UserExample
+	})
+	getByName(@Query('name') name: string) {
+		return this.userService.findByName(name);
+	}
+
+	@Get('/getByRole/')
+	@ApiOperation({
+		summary: 'Get users by Role'
+	})
+	@ApiQuery({
+		name: 'role',
+		type: 'string',
+		required: true,
+		examples: {
+			1: {
+				value: 'admin'
+			},
+			2: {
+				value: 'user'
+			},
+			3: {
+				value: 'other'
+			}
+		}
+	})
+	@ApiResponse({
+		status: 200,
+		description: 'User found',
+		type: Array<User>,
+		isArray: true,
+		example: UserExample
+	})
+	getByRole(@Query('role') role: UserRole) {
+		return this.userService.findByRole(role);
+	}
+
+	//PATCH
+
+	@Patch('updateOne/:id/')
+	@ApiOperation({
+		summary: 'Update one user by his ID'
+	})
+	@ApiParam({
+		name: 'id',
+		required: true,
+		type: 'string'
+	})
+	@ApiBody({
+		required: true,
+		isArray: false,
+		examples: {
+			1: {
+				value: {name: 'ExampleUserName', role: UserRole.ADMIN}
+			},
+			2: {
+				value: {
+					name: 'OtherUserName',
+					role: UserRole.USER
+				}
+			}
+		},
+		type: UpdateUserDto
+	})
+	@ApiResponse({
+		description: 'Updated user with new values',
+		example: UserExample
+	})
+	updateUser(@Param('id', new ParseUUIDPipe()) id: string, @Body() data: UpdateUserDto) {
+		if (!data.name?.length && !data.role?.length)
+			throw new BadRequestException('New user data must not be empty');
+		return this.userService.updateUser(id, data.name, data.role);
 	}
 
 	//POSTS
