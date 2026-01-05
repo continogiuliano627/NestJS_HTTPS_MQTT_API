10815466a41cb632bd41fccd69d21f43919df0ed
Giuliano Contino <continogiuliano627@gmail.com>
2026-01-05 13:23:57 -0300

    feat: added device module config

--- src/device/device.module.ts
+++ src/device/device.module.ts
@@ -1,4 +1,12 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {Device} from 'src/database/entities/device.entity';
+import {DeviceController} from './device.controller';
+import {DeviceService} from './device.service';
 
-@Module({})
+@Module({
+	imports: [TypeOrmModule.forFeature([Device])],
+	providers: [DeviceService],
+	controllers: [DeviceController]
+})
 export class DeviceModule {}
