7e349da38820349cf9752d911a0eba12253777ae
Giuliano Contino <continogiuliano627@gmail.com>
2026-01-17 00:59:01 -0300

    fix: fixed MQTT import

--- src/device/device.module.ts
+++ src/device/device.module.ts
@@ -1,11 +1,12 @@
 import {Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {Device} from 'src/database/entities/device.entity';
+import {MqttModule} from 'src/mqtt/mqtt.module';
 import {DeviceController} from './device.controller';
 import {DeviceService} from './device.service';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Device])],
+	imports: [TypeOrmModule.forFeature([Device]), MqttModule],
 	providers: [DeviceService],
 	controllers: [DeviceController]
 })
