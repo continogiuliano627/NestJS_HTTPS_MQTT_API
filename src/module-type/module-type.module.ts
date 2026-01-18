c15a595c4f5f2b97e92e82d7a182e717a06add83
Giuliano Contino <continogiuliano627@gmail.com>
2026-01-17 21:15:47 -0300

    feat: finished module-type.module settings

--- src/module-type/module-type.module.ts
+++ src/module-type/module-type.module.ts
@@ -1,4 +1,12 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ModuleType} from 'src/database/entities/module-type.entity';
+import {ModuleTypeController} from './module-type.controller';
+import {ModuleTypeService} from './module-type.service';
 
-@Module({})
+@Module({
+	imports: [TypeOrmModule.forFeature([ModuleType])],
+	providers: [ModuleTypeService],
+	controllers: [ModuleTypeController]
+})
 export class ModuleTypeModule {}
