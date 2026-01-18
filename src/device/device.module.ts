764b95e9644eaeea772ba0fda4d09e9ec0ec81d3
Giuliano Contino <continogiuliano627@gmail.com>
2026-01-17 21:11:09 -0300

    fix: fixed class import

--- src/device/device.module.ts
+++ src/device/device.module.ts
@@ -1,12 +1,13 @@
 import {Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import {Device_Module} from 'src/database/entities/device-module.entity';
 import {Device} from 'src/database/entities/device.entity';
 import {MqttModule} from 'src/mqtt/mqtt.module';
 import {DeviceController} from './device.controller';
 import {DeviceService} from './device.service';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Device]), MqttModule],
+	imports: [TypeOrmModule.forFeature([Device, Device_Module]), MqttModule],
 	providers: [DeviceService],
 	controllers: [DeviceController]
 })
