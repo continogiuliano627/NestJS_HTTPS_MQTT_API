9e3e5fdcf1ffdfa7511717b23f553c347480f644
Giuliano Contino <continogiuliano627@gmail.com>
2026-01-24 16:39:08 -0300

    feat: added mqtt request ack

--- src/mqtt/mqtt.service.ts
+++ src/mqtt/mqtt.service.ts
@@ -1,11 +1,18 @@
-import {Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	OnModuleDestroy,
+	OnModuleInit,
+	RequestTimeoutException
+} from '@nestjs/common';
 import * as mqtt from 'mqtt';
-import {MqttMessageHandler} from './mqtt.dto';
+import {MqttMessageHandler, mqttResp, PendingKey, PendingRequest} from './mqtt.dto';
 
 @Injectable()
 export class MqttService implements OnModuleInit, OnModuleDestroy {
 	private client: mqtt.MqttClient;
 	private handlers = new Set<MqttMessageHandler>();
+	private pending = new Map<PendingKey, PendingRequest>();
 
 	onModuleInit() {
 		this.client = mqtt.connect(process.env.MQTT_URL!, {
@@ -22,6 +29,34 @@ export class MqttService implements OnModuleInit, OnModuleDestroy {
 		});
 
 		this.client.on('message', (topic, payload) => {
+			const raw = payload.toString('utf8');
+			let parsed: unknown;
+			try {
+				parsed = JSON.parse(raw);
+			} catch {
+				return;
+			}
+			if (
+				typeof parsed === 'object' &&
+				parsed !== null &&
+				'id' in parsed &&
+				'action' in parsed &&
+				'pin' in parsed
+			) {
+				const msg = parsed as mqttResp;
+
+				if (msg.action !== 'update') return;
+				const key = `${msg.id}:${msg.pin}`;
+				const pending = this.pending.get(key);
+
+				if (pending) {
+					clearTimeout(pending.timeout);
+					pending.resolve(msg.value ?? '');
+					this.pending.delete(key);
+					return;
+				}
+			}
+
 			for (const handler of this.handlers) {
 				handler(topic, payload);
 			}
@@ -45,5 +80,33 @@ export class MqttService implements OnModuleInit, OnModuleDestroy {
 
 	onModuleDestroy() {
 		this.client?.end(true);
+	}
+
+	async publishWithAck(
+		topic: string,
+		message: {id: string; pin: string; action: 'read' | 'set'} & Record<string, unknown>,
+		timeoutMs = 5000
+	): Promise<string> {
+		if (message.id === 'device_report') {
+			this.client.publish(topic, JSON.stringify(message), {qos: 1});
+			return '';
+		}
+		if (message.action !== 'read' && message.action !== 'set')
+			throw new BadRequestException(
+				`Error send mqtt message: invalid action '${JSON.stringify(message.action)}'`
+			);
+		const key = `${message.id}:${message.pin}`;
+		if (this.pending.has(key)) throw new Error(`Pending request already exists for ${key}`);
+
+		return new Promise<string>((resolve, reject) => {
+			const timeout = setTimeout(() => {
+				this.pending.delete(key);
+				reject(new RequestTimeoutException(`ACK timeout for ${key}`));
+			}, timeoutMs);
+
+			this.pending.set(key, {resolve, reject, timeout});
+
+			this.client.publish(topic, JSON.stringify(message), {qos: 1});
+		});
 	}
 }
