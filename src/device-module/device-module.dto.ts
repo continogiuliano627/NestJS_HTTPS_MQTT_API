f6ea823e2b7160e38fcfba4ecd36610f56e905db
Giuliano Contino <continogiuliano627@gmail.com>
2026-01-23 22:27:37 -0300

    feat: updated DTO's

--- src/device-module/device-module.dto.ts
+++ src/device-module/device-module.dto.ts
@@ -1,24 +1,33 @@
-import type {Device_Module} from 'src/database/entities/device-module.entity';
+import type {Device} from 'src/database/entities/device.entity';
+import type {ModuleType} from 'src/database/entities/module-type.entity';
+import {DeviceExample} from 'src/device/device.dto';
+import {ModuleTypeExample} from 'src/module-type/module-type.dto';
 
-export const DeviceModuleExample: Device_Module = {
+export class Device_ModuleDTO {
+	id: string;
+	name: string;
+	deviceId: string;
+	device: Device;
+	typeId: string;
+	type: ModuleType;
+}
+
+export const DeviceModuleExample: Device_ModuleDTO = {
 	id: '550e8400-e29b-41d4-a716-446655440000',
 	name: 'Luces cocina',
-
 	deviceId: 'CC:50:E3:47:D1:DF',
-	device: {
-		id: 'CC:50:E3:47:D1:DF',
-		name: 'ESP Cocina',
-		isDeleted: false,
-		createdAt: new Date('2026-01-17T10:15:00.000Z'),
-		updatedAt: new Date('2026-01-17T10:15:00.000Z'),
-		modules: []
-	},
-
+	device: DeviceExample,
 	typeId: '2f3a9c7e-91a1-4b6a-b8b1-3d0f2a9e8c44',
-	type: {
-		id: '2f3a9c7e-91a1-4b6a-b8b1-3d0f2a9e8c44',
-		name: 'Relay mecánico',
-		kind: 'ACT',
-		isDeleted: false
-	}
+	type: ModuleTypeExample
 };
+
+export class CreateDeviceModuleDTO {
+	name: string;
+	deviceId: string;
+	typeId: string;
+}
+
+export class UpdateDeviceModuleDTO {
+	name?: string;
+	typeId?: string;
+}
