7067bfaa9b6a9468a054040f555a02762d25b7d5
Giuliano Contino <continogiuliano627@gmail.com>
2026-02-02 14:59:46 -0300

    feat: added icon and pin prop

--- src/device-module/device-module.dto.ts
+++ src/device-module/device-module.dto.ts
@@ -10,24 +10,32 @@ export class Device_ModuleDTO {
 	device: Device;
 	typeId: string;
 	type: ModuleType;
+	pin: string;
+	icon: number;
 }
 
 export const DeviceModuleExample: Device_ModuleDTO = {
 	id: '550e8400-e29b-41d4-a716-446655440000',
 	name: 'Luces cocina',
+	pin: '2',
 	deviceId: 'CC:50:E3:47:D1:DF',
 	device: DeviceExample,
 	typeId: '2f3a9c7e-91a1-4b6a-b8b1-3d0f2a9e8c44',
-	type: ModuleTypeExample
+	type: ModuleTypeExample,
+	icon: -1
 };
 
 export class CreateDeviceModuleDTO {
 	name: string;
+	pin: string;
 	deviceId: string;
 	typeId: string;
+	icon: number;
 }
 
 export class UpdateDeviceModuleDTO {
 	name?: string;
 	typeId?: string;
+	pin?: string;
+	icon?: number;
 }
